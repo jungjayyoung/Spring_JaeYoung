27d8cc7c0b5d8a7f4e2cbf9af21999535a4de996
jayyoung <40835055+jungjayyoung@users.noreply.github.com>
2022-02-13 23:52:16 +0900

    first commit

---
+++ src/test/java/young/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package young.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
